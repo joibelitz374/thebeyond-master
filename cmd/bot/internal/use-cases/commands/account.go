e48ef8c37d2e91c4062903b69b2b89a4a60b8a92
muffin <muffin137@snglr.social>
2025-12-14 03:49:30 +0300

    💬 Domain added to environment variable

--- cmd/bot/internal/use-cases/commands/account.go
+++ cmd/bot/internal/use-cases/commands/account.go
@@ -70,7 +70,7 @@ func (h accountHandler) activeAccountInfo(bot bin.Interface, p *domain.Payload, 
 		standard.WithLogoImageFilePNG(os.Getenv("PROJECT_AVATAR_PATH")),
 	}
 
-	subscriptionURL := "https://axyvpn.online/sub/" + p.Account.KeyID
+	subscriptionURL := "https://" + os.Getenv("PROJECT_DOMAIN") + "/sub/" + p.Account.KeyID
 	qrc, err := qrcode.New(subscriptionURL)
 	if err != nil {
 		return fmt.Errorf("could not generate QRCode: %v", err)
