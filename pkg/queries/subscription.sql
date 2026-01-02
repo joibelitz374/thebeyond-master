410915dc220d8e917b44bc1556df0cec057be3f2
joibelitz374 <joibelitz374@hotmail.com>
2026-01-02 19:45:59 +0300

    🐛 fix: traffic 4 changed to traffic 3 in the GetAccountsToEnable query

--- pkg/queries/subscription.sql
+++ pkg/queries/subscription.sql
@@ -37,7 +37,7 @@ SELECT id, key_id FROM (
             (CASE
                 WHEN subscription_status = 'available'
                     THEN CASE tariff
-                        WHEN 1 THEN 150 WHEN 2 THEN 400 WHEN 4 THEN 5120
+                        WHEN 1 THEN 150 WHEN 2 THEN 400 WHEN 3 THEN 5120
                         ELSE 0
                     END
                 ELSE 0
